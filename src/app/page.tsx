e402eb4f01febbaa76f9b2528cdd8a18c06dd251
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-06-04 12:31:27 +0530

    next app initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center bg-neutral-400 max-w-full h-screen">
+      <div >
+             My portFolio 
+      </div>
+          
+    </div>
+  );
+}
