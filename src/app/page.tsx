e71b985bddad1cabc6a04218332cc2ea9f920c39
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-07-28 12:00:24 +0530

    deleled files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import About from "@/components/About";
-import { Navbar } from "@/components/Navbar";
-import ProjectsSection from "@/components/ProjectSection";
+
 
 export default function Home() {
   return (
   <div className="flex flex-col justify-center bg-neutral-300 w-full p-10 gap-y-4"> {/* Added gap-y-4 */}
-    <Navbar/>  
-    <About/>
-    <ProjectsSection/>
+      Hello World!
   </div>
   );
 }
