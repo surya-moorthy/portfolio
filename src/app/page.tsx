717f492fe360216610cca786323a2f982588714a
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-09-03 21:51:28 +0530

    added portfolio structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,44 @@
-
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="min-h-[200vh]">
-    <div className="flex flex-col min-h-screen justify-center items-center bg-neutral-300 w-full p-10 gap-y-4 "> {/* Added gap-y-4 */}
-     
-     <h1 className="text-4xl font-bold ">
-      Surya Moorthy
-     </h1>
-     <p className="text-xl font-semibold text-neutral-500 tracking-tight max-w-lg text-center">
-      passionate full stack and web3 developer, explore new tools and build proof of work
-     </p>
-   </div>
+  <div className="flex flex-col justify-center items-center p-10 max-w-full lg:max-w-2xl">
+       <Navbar/>
+       <div className="flex justify-center items-center gap-10 px-8">
+             <Image src={""} alt=""/>
+             <div>
+                  <h3>Surya Moorthy</h3>
+                  <p>
+                      Description
+                  </p>
+                  <div>
+                       <span>
+                          Reach me here:
+                      </span>
+                      <div>
+                            github
+                            instagram
+                            linkedin
+                            X
+                      </div>
+                  </div>
+             </div>
+       </div>
+
+       <div>
+             <h3> Tech Stack</h3>
+              <div>
+                   buttons of techie
+              </div>
+       </div>
+
+       <div>
+            <h3> 
+               Projects
+            </h3>
+            <ProjectCard/>
+       </div>
   </div>
   );
 }
